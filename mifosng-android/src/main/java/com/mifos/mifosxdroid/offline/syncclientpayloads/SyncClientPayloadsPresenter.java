1b10fab0533754028071137b24c309a95788695a
Rajan Maurya <rajanmaurya154@gmail.com>
2016-07-06 15:45:57 +0530

    CreateNewClient fix RxAndroid Calls
    Removed Unsed variables
    
    CompositeSubscription TODO added
    
    getClientTemplate shifted in DataManagerClient to DataManager
    
    ClientTemplate Annotation done
    
    ClientTemplate saing in Database
    
    ClientTemplate Tables named
    
    ClientTemplate saving in DataBase
    
    Client Template saved and retrive successfully
    
    Offices Saving and retrieving from DB successfully
    
    All check passing
    
    Offices Staff saving and retrieving in Database
    
    working on Client Payload
    
    Client creating online and in offline mode saving into data successfully
    
    String Response Handle of online and offline
    
    Sync Fragment added with all logic
    
    fix
    
    client payload creating one time done
    
    updating client index
    
    clients syncing done
    
    Client Payload sync done with documentation
    
    package name fix
    
    Documentation added .
    
    SyncClientPayloadtest done !

---
+++ mifosng-android/src/main/java/com/mifos/mifosxdroid/offline/syncclientpayloads/SyncClientPayloadsPresenter.java
@@ -0,0 +1,124 @@
+package com.mifos.mifosxdroid.offline.syncclientpayloads;
+
+import com.mifos.api.datamanager.DataManagerClient;
+import com.mifos.mifosxdroid.base.BasePresenter;
+import com.mifos.objects.client.Client;
+import com.mifos.objects.client.ClientPayload;
+
+import java.util.List;
+
+import javax.inject.Inject;
+
+import rx.Observer;
+import rx.Subscriber;
+import rx.Subscription;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
+
+/**
+ * Created by Rajan Maurya on 08/07/16.
+ */
+public class SyncClientPayloadsPresenter extends BasePresenter<SyncClientPayloadsMvpView> {
+
+
+    private final DataManagerClient mDataManagerClient;
+    private Subscription mSubscription;
+
+    @Inject
+    public SyncClientPayloadsPresenter(DataManagerClient dataManagerClient) {
+        mDataManagerClient = dataManagerClient;
+    }
+
+    @Override
+    public void attachView(SyncClientPayloadsMvpView mvpView) {
+        super.attachView(mvpView);
+    }
+
+    @Override
+    public void detachView() {
+        super.detachView();
+        if (mSubscription != null) mSubscription.unsubscribe();
+    }
+
+    public void loadDatabaseClientPayload() {
+        checkViewAttached();
+        getMvpView().showProgressbar(true);
+        if (mSubscription != null) mSubscription.unsubscribe();
+        mSubscription = mDataManagerClient.getAllDatabaseClientPayload()
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeOn(Schedulers.io())
+                .subscribe(new Subscriber<List<ClientPayload>>() {
+                    @Override
+                    public void onCompleted() {
+
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        getMvpView().showError("Failed to load ClientPayload");
+                    }
+
+                    @Override
+                    public void onNext(List<ClientPayload> clientPayloads) {
+                        getMvpView().showProgressbar(false);
+                        getMvpView().showPayloads(clientPayloads);
+                    }
+                });
+    }
+
+    public void syncClientPayload(ClientPayload clientPayload) {
+        checkViewAttached();
+        getMvpView().showProgressbar(true);
+        if (mSubscription != null) mSubscription.unsubscribe();
+        mSubscription = mDataManagerClient.createClient(clientPayload)
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeOn(Schedulers.io())
+                .subscribe(new Observer<Client>() {
+                    @Override
+                    public void onCompleted() {
+
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        getMvpView().showProgressbar(false);
+                        getMvpView().showClientSyncFailed();
+                    }
+
+                    @Override
+                    public void onNext(Client client) {
+                        getMvpView().showProgressbar(false);
+                        getMvpView().showSyncResponse();
+                    }
+                });
+    }
+
+
+    public void deleteAndUpdateClientPayload(int id) {
+        checkViewAttached();
+        getMvpView().showProgressbar(true);
+        if (mSubscription != null) mSubscription.unsubscribe();
+        mSubscription = mDataManagerClient.deleteAndUpdatePayloads(id)
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeOn(Schedulers.io())
+                .subscribe(new Observer<List<ClientPayload>>() {
+                    @Override
+                    public void onCompleted() {
+
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        getMvpView().showProgressbar(false);
+                        getMvpView().showError("Failed Update List");
+                    }
+
+                    @Override
+                    public void onNext(List<ClientPayload> clientPayloads) {
+                        getMvpView().showProgressbar(false);
+                        getMvpView().showPayloadDeletedAndUpdatePayloads(clientPayloads);
+                    }
+                });
+    }
+
+}
